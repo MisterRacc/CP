bc7c023a4e2aadb995b0667521533d3d006a32ad
Fábio Nunes <fabiodiogo.an111@gmail.com>
2023-11-13 21:10:53 +0000

    <feat>: código para o Joystick

---
+++ Assets/scripts/Joystick.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystick : MonoBehaviour
+{
+    public Transform player;
+    public float speed;
+    private bool touchStart = false;
+    private Vector2 pointA;
+    private Vector2 pointB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // mexer o quadrado com as setas ou wasd
+        // moveCharacter(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+
+        // mexer quando seguras com o rato na tela ou com os dedos (😏)
+        if(Input.GetMouseButtonDown(0)){
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+        }
+
+        if(Input.GetMouseButton(0)){
+            touchStart = true;
+            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+        }
+        else{
+            touchStart = false;
+        }
+    }
+
+    private void FixedUpdate(){
+        if(touchStart){
+            Vector2 offset = pointB - pointA;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            moveCharacter(direction);
+        }
+    }
+
+    // https://www.youtube.com/watch?v=uVxnvXonGXY fiquei nos 8 min mas tmb é imagens para o joystick depois vê-se isso
+
+    void moveCharacter(Vector2 direction){
+        player.Translate(direction*speed*Time.deltaTime);
+    }
+}
