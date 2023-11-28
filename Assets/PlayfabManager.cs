332a1b792ab684dc6489a3d4b18792a3be5e9236
KillingSpread1 <2081020@student.uma.pt>
2023-11-28 22:41:39 +0000

    Finally implemented the Register requirement

--- Assets/PlayfabManager.cs
+++ Assets/PlayfabManager.cs
@@ -1,46 +1,61 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using PlayFab;
-// using PlayFab.ClientModels;
-// using Newtonsoft.Json;
-// using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using Newtonsoft.Json;
+using UnityEngine.UI;
 
-// public class PlayfabManager : MonoBehaviour
-// {
+public class PlayfabManager : MonoBehaviour
+{
 
-//     public Text messageText;
+    public Text messageText;
 
-//     public InputField usernameInput, passwordInput;
+    public InputField usernameInput, passwordInput;
 
-//     public void RegisterButton(){
-//         var request = new RegisterPlayFabUserRequest{
-//             Username = usernameInput.text,
-//             Password = passwordInput.text,
-//             RequireBothUsernameAndEmail = false
-//         };
-//         PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
-//     }
+    public void RegisterButton(){
+        if (usernameInput != null && passwordInput != null)
+        {      
+            if (passwordInput.text.Length < 6){
+                messageText.text = "Password must be at least 6 characters long.";
+                return;
+            }
+            var request = new RegisterPlayFabUserRequest{
+                Username = usernameInput.text,
+                Password = passwordInput.text,
+                RequireBothUsernameAndEmail = false
+            };
+            PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+        }
+        else{
+            Debug.LogError("Username or password input field is empty!");
+        }
+    }
 
-//     void OnRegisterSuccess(RegisterPlayFabUserResult result){
-//         messageText.text = "Registered and logged in!";
-//     }
+    void OnRegisterSuccess(RegisterPlayFabUserResult result){
+        messageText.text = "Registered and logged in!";
+    }
+
+    void OnError(PlayFabError error){
+        messageText.text = error.ErrorMessage;
+        Debug.LogError(error.GenerateErrorReport());
+    }
 
 
-//     public void LoginButton(){
+    public void LoginButton(){
 
-//     }
+    }
 
-//     // Start is called before the first frame update
-//     void Start()
-//     {
+    // Start is called before the first frame update
+    void Start()
+    {
         
-//     }
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
+    // Update is called once per frame
+    void Update()
+    {
         
-//     }
-// }
+    }
+}
 
