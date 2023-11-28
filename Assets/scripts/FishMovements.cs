2588b3ec8d21d874ac171d3f75020504206057ec
Fábio Nunes <fabiodiogo.an111@gmail.com>
2023-11-28 18:08:31 +0000

    entusiasmei-me para alem do movimento do fio de pesca tmb fiz o spawn dos peixes
    para o nível 3 todo vou continuar nesta branch fuck it

---
+++ Assets/scripts/FishMovements.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+
+public class FishMovements : MonoBehaviour
+{
+    public float speed;
+    public float changeDirectionInterval;
+
+    private float timeSinceLastDirectionChange;
+    private int isMovingUp;
+    private int isMovingRight;
+    private bool spawnFromLeft;
+    
+    private float minY = 100f;
+    private float maxY = 800f;
+
+    void Start()
+    {
+        timeSinceLastDirectionChange = changeDirectionInterval;
+
+        spawnFromLeft = Random.Range(0, 2) == 1 ? true : false;
+        if(spawnFromLeft) isMovingRight = GameObject.FindGameObjectWithTag("LeftSpawner").GetComponent<LeftFishSpawner>().MoveDirection();
+        else isMovingRight = GameObject.FindGameObjectWithTag("RightSpawner").GetComponent<RightFishSpawner>().MoveDirection();
+
+        Debug.Log(spawnFromLeft);
+    }
+
+    void Update()
+    {
+        MoveInsideTheMap();
+    }
+
+    void MoveInsideTheMap()
+    {
+        timeSinceLastDirectionChange += Time.deltaTime;
+
+        if (timeSinceLastDirectionChange >= changeDirectionInterval)
+        {
+            isMovingUp = Random.Range(0, 2) == 1 ? 1 : -1;
+            timeSinceLastDirectionChange = 0;
+        }
+
+        transform.position += Vector3.up*speed*isMovingUp*Time.deltaTime;
+        transform.position += Vector3.right*speed*isMovingRight*Time.deltaTime;
+
+        ClampPosition();
+    }
+
+    void ClampPosition()
+    {
+        float clampedY = Mathf.Clamp(transform.position.y, minY, maxY);
+        transform.position = new Vector3(transform.position.x, clampedY, transform.position.z);
+    }
+}
