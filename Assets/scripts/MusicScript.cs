8fbefd1a3b9b33982457ee05acf5b10a4e96c1ba
Rúben Campos <2078820@student.uma.pt>
2023-12-14 13:18:15 +0000

    Fixed UI to go forward, added musics, fixed Configuration

--- Assets/scripts/MusicScript.cs
+++ Assets/scripts/MusicScript.cs
@@ -7,6 +7,10 @@ public class LevelMusicScript : MonoBehaviour
     public AudioClip defaultMusic;
     public AudioClip level1Music;
     public AudioClip level2Music;
+    public AudioClip level3Music;
+    public AudioClip level4Music;
+    public AudioClip level5Music;
+    public AudioClip level6Music;
 
     void Start()
     {
@@ -25,6 +29,18 @@ public class LevelMusicScript : MonoBehaviour
                     break;
                 case "Level 2":
                     AudioManager.Instance.PlayLevelMusic(level2Music);
+                    break;
+                case "Level 3":
+                    AudioManager.Instance.PlayLevelMusic(level3Music);
+                    break;
+                case "Level 4":
+                    AudioManager.Instance.PlayLevelMusic(level4Music);
+                    break;
+                case "Level 5":
+                    AudioManager.Instance.PlayLevelMusic(level5Music);
+                    break;
+                case "Level 6":
+                    AudioManager.Instance.PlayLevelMusic(level6Music);
                     break;
                 default:
                     // Default case, can be used for a general level or fallback music
