d0370611a1bcfb1d7d4dc3a7eb366196efea50e8
FranciscoBelo18 <96338080+FranciscoBelo18@users.noreply.github.com>
2023-12-15 00:10:19 +0000

    Done animations on lvl3

--- Assets/scripts/LeftSpawner.cs
+++ Assets/scripts/LeftSpawner.cs
@@ -30,7 +30,7 @@ public class LeftSpawner : MonoBehaviour
 
         MovementsUnderwater mu = entity.GetComponent<MovementsUnderwater>();
         mu.DefineSpawn(true);
-        entity.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+        entity.transform.SetParent(GameObject.FindGameObjectWithTag("Spawner").transform, false);
         entity.transform.position = new Vector3(spawnX, spawnY, 0);
     }
 }
